d015aca42725cd66adb9eff315bcca2b8c60c50b
wadamiru <wadamiru@gmail.com>
2026-03-06 20:11:55 +1100

    fixing file structure

--- projects/linear-regression/src/data.py
+++ projects/linear-regression/src/data.py
@@ -5,7 +5,7 @@ def make_data(w, b, nsamples=100, noise=False, noise_std=1.0):
     Generate linear data: y = w*x + b (+ optional Gaussian noise)
     """
     x = np.random.rand(nsamples)
-    y = w * x + b
+    y = w*x + b
     
     if noise:
         eps = np.random.normal(0, noise_std, size=nsamples)
